76c3b90e094fa028a336a631a9a5f1734c0f1c68
dromriell <devonromriell@gmail.com>
2022-06-14 22:36:48 -0400

    - Add additional img src fields

--- screens/models.py
+++ screens/models.py
@@ -5,7 +5,9 @@ from projects.models import Project
 class Screen(models.Model):
    name = models.CharField(max_length=50)
    description = models.TextField(blank=True, null=True)
-   img_source = models.URLField(max_length=200, blank=True, null=True)
+   bg_img_source = models.URLField(max_length=200, blank=True, null=True)
+   img_1_source = models.URLField(max_length=200, blank=True, null=True)
+   img_2_source = models.URLField(max_length=200, blank=True, null=True)
    apps = models.ManyToManyField(Project, related_name='apps', blank=True)
    index = models.PositiveIntegerField(blank=True, null=True, unique=True)
    background_color = models.CharField(max_length=7, default="#ffffff")
