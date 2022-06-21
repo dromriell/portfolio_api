efea23800c8027facce5e6d070431c8a75fdd9de
dromriell <devonromriell@gmail.com>
2022-06-21 14:55:59 -0400

    - Add test server to cors origins

--- portfolio_api/settings.py
+++ portfolio_api/settings.py
@@ -141,7 +141,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5000",
     'http://192.168.4.39:3000',
     'http://192.168.4.39:5000',
-    'https://dromriell-portfolio.herokuapp.com',
+    "https://62b2139aaf335c1816efab97--dromriell-portfolio.netlify.app"
 ]
 
 CORS_ALLOW_HEADERS = list(default_headers) + [
