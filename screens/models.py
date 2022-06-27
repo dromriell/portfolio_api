dac5a7c73f43c2f96d9ebcfb7afaeb99852d06f2
dromriell <devonromriell@gmail.com>
2022-06-26 20:10:31 -0400

    - Add video source field to screen

--- screens/models.py
+++ screens/models.py
@@ -8,6 +8,7 @@ class Screen(models.Model):
    bg_img_source = models.URLField(max_length=200, blank=True, null=True)
    img_1_source = models.URLField(max_length=200, blank=True, null=True)
    img_2_source = models.URLField(max_length=200, blank=True, null=True)
+   video_source = models.URLField(max_length=200, blank=True, null=True)
    apps = models.ManyToManyField(Project, related_name='apps', blank=True)
    index = models.PositiveIntegerField(blank=True, null=True, unique=True)
    background_color = models.CharField(max_length=7, default="#ffffff")
