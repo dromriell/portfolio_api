cbaae0d7b3b8283a650bf06539c2ec29d0e38bf9
dromriell <devonromriell@gmail.com>
2022-06-28 13:40:21 -0400

    - Create admin for notes

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Note
+   
+class NoteAdmin(admin.ModelAdmin):
+   model = Note
 
-# Register your models here.
+admin.site.register(Note, NoteAdmin)
